9e15dd06069b62d462d1ca907b9016c2f15e22c5
sangwon-jung <tkddnjs11122@naver.com>
2025-09-08 10:17:05 +0900

    weather.py 오류 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from app.routers import posts
 from app.db.database import Base, engine
-from app.routers import posts, upload
+from app.routers import posts, upload, weather
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
@@ -21,6 +20,7 @@ app.add_middleware(
 
 app.include_router(posts.router, prefix="/posts", tags=["posts"])
 app.include_router(upload.router, prefix="/upload", tags=["upload"])
+app.include_router(weather.router, prefix="/weather", tags=["weather"])
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
--- backend/app/routers/weather.py
+++ backend/app/routers/weather.py
@@ -6,7 +6,7 @@ router = APIRouter()
 # 실제로는 환경 변수에 저장하는 것이 좋습니다.
 API_KEY = "5d88d6a2b12722c05e25f135c492cb0b"
 
-router.get("/weather")
+@router.get("/")
 async def get_weather(city: str):
     # 외부 날씨 API 호출
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
